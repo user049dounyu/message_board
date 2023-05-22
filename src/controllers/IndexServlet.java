6b2389592be219ca69f9a901cea8469dc92eddac
korechika.takayama <mahiro.nino@techacademy.jp>
2023-05-22 11:02:47 +0900

    フラッシュメッセージを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -38,8 +38,15 @@ public class IndexServlet extends HttpServlet {
         List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
 
         em.close();
-   
+
         request.setAttribute("messages", messages);
+
+        // フラッシュメッセージがセッションスコープにセットされていたら
+        // リクエストスコープに保存する（セッションスコープからは削除）
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
         rd.forward(request, response);
